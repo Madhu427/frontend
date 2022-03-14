fd4e52298827bf9c89ed04ba7833ca9efef3ff4f
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-14 19:27:24 +0530

    aws_instance instead of spot

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance "ec2-inst
+resource "aws_spot_instance_request" "ec2-spot" {
   count = var.INSTANCE_COUNT
   ami           = data.aws_ami.ami.id
   instance_type = var.INSTANCE_TYPE
