aef38277c94fbaf4985dd457dea03a3c2aea0d45
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-14 23:30:39 +0530

    aws_instance instead of spot

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "ec2-inst" {
-  count = var.INSTANCE_COUNT
+  count         = var.INSTANCE_COUNT
   ami           = data.aws_ami.ami.id
   instance_type = var.INSTANCE_TYPE
 
   tags = {
-    Name = "${var.COMPONENT}-${var.ENV}-${count.index+1}"
+    Name = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
   }
 }
 
