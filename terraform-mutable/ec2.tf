0ef30cb82326387cc41597c0b5c39080465587cd
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-15 13:52:32 +0530

    aws_ec2_tag

--- terraform-mutable/ec2.tf
+++ terraform-mutable/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "ec2-spot" {
   ami           = data.aws_ami.ami.id
   instance_type = var.INSTANCE_TYPE
   tags = {
-    Name = "${var.COMPONENT}-${var.ENV}-${count.index+1}"
+    Name = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
   }
 }
 
@@ -11,5 +11,5 @@ resource "aws_ec2_tag" "spot-instances" {
   count       = length(aws_spot_instance_request.ec2-spot)
   resource_id = aws_spot_instance_request.ec2-spot.*.id[count.index]
   key         = "Name"
-  value       = "${var.COMPONENT}-${var.ENV}-${count.index+1}"
+  value       = "${var.COMPONENT}-${var.ENV}-${count.index + 1}"
 }
