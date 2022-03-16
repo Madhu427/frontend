4d8f8f2968ad1038a55542c84d015061abbbb7ac
Madhu427 <madhusudhanachary.k@gmail.com>
2022-03-16 17:08:44 +0530

    frontend--> app-deploy

--- terraform-mutable/data.tf
+++ terraform-mutable/data.tf
@@ -22,4 +22,10 @@ data "terraform_remote_state" "alb" {
   }
 }
 
+data "aws_secretsmanager_secret" "common2" {
+  name = "common2/ssh"
+}
 
+data "aws_secretsmanager_secret_version" "secrets" {
+  secret_id = data.aws_secretsmanager_secret.common2.id
+}
